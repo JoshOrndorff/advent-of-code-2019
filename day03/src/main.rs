37a2ab18bd22c21dab5cbc63f0d2e2328e52e143
Joshy Orndorff <admin@joshyorndorff.com>
2019-12-04 15:16:06 -0500

    Freshly solved 3.2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::{HashMap, HashSet};
+use std::convert::TryInto;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashSet;
-use std::convert::TryInto;
 
 #[derive(Eq, PartialEq, Hash, Copy, Clone, Debug)]
 struct Point(isize, isize);
@@ -23,24 +23,43 @@ struct Segment {
 fn main() {
     // Read data from file
     let f = BufReader::new(File::open("input.txt").unwrap());
-    let paths: Vec<_> = f
+
+    let wires_steps: Vec<HashMap<Point, usize>> = f
         .lines()
         .map(|l| l.unwrap())
+        .map(|x| parse_path(&x))
+        .map(|x| path_to_steps(&x))
         .collect();
 
-    // Calcualte
-    let shortest = part1(&paths[0], &paths[1]);
+    let wires_points: Vec<HashSet<Point>> = wires_steps
+        .iter()
+        .map(|steps| steps.keys().map(|x| *x).collect::<HashSet<Point>>())
+        .collect();
+    let intersection_points: HashSet<Point> = wires_points[0]
+        .intersection(&wires_points[1])
+        .map(|x| *x)
+        .collect();
 
     // Print result
-    println!("smallest distance: {:?}", shortest);
+    println!("smallest distance: {}", part1(&intersection_points));
+    println!(
+        "fewest combined steps: {}",
+        part2(&intersection_points, &wires_steps)
+    );
 }
 
-fn part1(path1: &str, path2: &str) -> usize {
-    let points1 = path_to_points(&parse_path(path1));
-    let points2 = path_to_points(&parse_path(path2));
-    let intersection_points = points1.intersection(&points2);
+fn part2(points: &HashSet<Point>, step_maps: &Vec<HashMap<Point, usize>>) -> usize {
+    points
+        .iter()
+        .map(|p| step_maps[0].get(&p).unwrap() + step_maps[1].get(&p).unwrap())
+        .min()
+        .unwrap()
+}
 
-    intersection_points.map(manhattan_distance).min().unwrap()
+/// Solve part 1 by calculating the Manhattan distance for each point in the set
+/// and finding the shortest
+fn part1(points: &HashSet<Point>) -> usize {
+    points.iter().map(manhattan_distance).min().unwrap()
 }
 
 //TODO Maybe nom would be better for this stuff
@@ -50,44 +69,51 @@ fn parse_path(s: &str) -> Vec<Segment> {
 
 fn parse_segment(s: &str) -> Segment {
     let letter = s.chars().nth(0).unwrap();
-    let direction = if letter == 'U' { Direction::U }
-               else if letter == 'D' { Direction::D }
-               else if letter == 'L' { Direction::L }
-               else if letter == 'R' { Direction::R }
-               else {panic!("Unrecognized direction")};
+    let direction = if letter == 'U' {
+        Direction::U
+    } else if letter == 'D' {
+        Direction::D
+    } else if letter == 'L' {
+        Direction::L
+    } else if letter == 'R' {
+        Direction::R
+    } else {
+        panic!("Unrecognized direction")
+    };
 
     let length = s.chars().skip(1).collect::<String>().parse().unwrap();
 
-    Segment{
-        direction,
-        length,
-    }
+    Segment { direction, length }
 }
 
-fn path_to_points(segments: &Vec<Segment>) -> HashSet<Point> {
-    let mut cur = Point(0,0);
-    let mut points = HashSet::new();
+/// Takes in a path reference and returns a HashMap mapping
+/// each point traversed to the number of steps it took to get there.
+fn path_to_steps(segments: &Vec<Segment>) -> HashMap<Point, usize> {
+    let mut cur_x = 0;
+    let mut cur_y = 0;
+    let mut steps = 1;
+    let mut points = HashMap::new();
 
     for segment in segments.iter() {
         for _ in 0..segment.length {
             match segment.direction {
                 Direction::U => {
-                    cur = Point(cur.0, cur.1 + 1);
-                },
+                    cur_y += 1;
+                }
                 Direction::D => {
-                    cur = Point(cur.0, cur.1 - 1);
-                },
+                    cur_y -= 1;
+                }
                 Direction::L => {
-                    cur = Point(cur.0 - 1, cur.1);
-                },
+                    cur_x -= 1;
+                }
                 Direction::R => {
-                    cur = Point(cur.0 + 1, cur.1);
-                },
+                    cur_x += 1;
+                }
             }
-            points.insert(cur.clone());
+            points.insert(Point(cur_x, cur_y), steps);
+            steps += 1;
         }
     }
-
     points
 }
 
@@ -106,11 +132,23 @@ mod test {
 
     #[test]
     fn ex1_1() {
-        assert_eq!(part1("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"), 159);
+        assert_eq!(
+            part1(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+            ),
+            159
+        );
     }
 
     #[test]
     fn ex1_2() {
-        assert_eq!(part1("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"), 135);
+        assert_eq!(
+            part1(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            ),
+            135
+        );
     }
 }
