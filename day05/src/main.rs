37292f49d9c38a62f9404362fd84b73678292852
Joshy Orndorff <admin@joshyorndorff.com>
2019-12-07 00:38:36 -0500

    Attempt at 5.1. Probably close.

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,19 @@
+// 224 is too low
+
+use std::fs;
+use intcode::Intcode;
+use std::collections::VecDeque;
+
 fn main() {
-    println!("Hello, world!");
+    // Read input file to string
+    let s = fs::read_to_string("input.txt").unwrap();
+
+    // Compute Results
+    let mut input = VecDeque::new();
+    input.push_back(1);
+    let mut machine = Intcode::new_with_input(&s, input);
+    machine.execute();
+
+    // Print results
+    println!("TEST output: {:?}", machine.get_output());
 }
--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,11 +1,29 @@
+use std::collections::VecDeque;
+
+// hardcoded lengths of opcodes.
+// opcodes 1 and 2 have length four (see day 2)
+// opcodes 3 and 4 have length two (see day 5)
+const lengths: [usize; 5] = [0, 4, 4, 2, 2];
+
+#[derive(Eq, PartialEq)]
+enum Mode {
+    Position,
+    Immediate,
+}
+
+#[derive(Eq, PartialEq)]
+struct Operation {
+    opcode: usize,
+    modes: Vec<Mode>,
+}
 
 #[derive(Eq, PartialEq)]
 pub struct Intcode {
     memory: Vec<isize>,
     pointer: usize,
     halted: bool,
-    input: Vec<isize>,
-    output: Vec<isize>,
+    input: VecDeque<isize>,
+    output: VecDeque<isize>,
 }
 
 impl Intcode {
@@ -25,36 +43,81 @@ impl Intcode {
             // println!("prestate: {:?}", tape);
 
             let opcode = self.memory[self.pointer];
-            let operand1 = self.memory[self.memory[self.pointer + 1] as usize];
-            let operand2 = self.memory[self.memory[self.pointer + 2] as usize];
-            let result_cell = self.memory[self.pointer + 3];
+            let operation = parse_operation(opcode);
 
-            self.memory[result_cell as usize] = if opcode == 1 {
+            if operation.opcode == 1 {
                 // Add instruction
-                operand1 + operand2
-            } else if opcode == 2 {
+                let operand1 = self.memory[
+                    match operation.modes[0] {
+                        Mode::Position => self.memory[self.pointer + 1] as usize,
+                        Mode::Immediate => self.pointer + 1,
+                    }
+                ];
+                let operand2 = self.memory[
+                    match operation.modes[1] {
+                        Mode::Position => self.memory[self.pointer + 2] as usize,
+                        Mode::Immediate => self.pointer + 2,
+                    }
+                ];
+                // I'm not even looking at the mode here. It only makes sense
+                // for the result cell to be location mode
+                let result_cell = self.memory[self.pointer + 3] as usize;
+
+                self.memory[result_cell] = operand1 + operand2;
+
+            } else if operation.opcode == 2 {
                 // Multiply instruction
-                operand1 * operand2
+                //TODO lots of duplicated code with addition
+                let operand1 = self.memory[
+                    match operation.modes[0] {
+                        Mode::Position => self.memory[self.pointer + 1] as usize,
+                        Mode::Immediate => self.pointer + 1,
+                    }
+                ];
+                let operand2 = self.memory[
+                    match operation.modes[1] {
+                        Mode::Position => self.memory[self.pointer + 2] as usize,
+                        Mode::Immediate => self.pointer + 2,
+                    }
+                ];
+                // I'm not even looking at the mode here. It only makes sense
+                // for the result cell to be location mode
+                let result_cell = self.memory[self.pointer + 3] as usize;
+
+                self.memory[result_cell] = operand1 * operand2;
+
+            } else if operation.opcode == 3 {
+                // Input instruction
+                let input_value = self.input.pop_front().unwrap();
+                // I'm not even looking at the mode here. It only makes sense
+                // for the result cell to be location mode
+                let result_cell = self.memory[self.pointer + 1] as usize;
+                self.memory[result_cell] = input_value;
+            } else if operation.opcode == 4 {
+                // Output instruction
+                // I'm not even looking at the mode here. It does make sense to
+                // output an immediate, but the instructions don't describe that happening
+                let output_value = self.memory[self.pointer + 1];
+                self.output.push_back(output_value);
             } else {
                 panic!("Invalid opcode: {}", opcode)
             };
 
             // println!("poststate: {:?}", self.memory);
 
-            self.pointer += 4;
+            self.pointer += lengths[operation.opcode];
         }
 
         self.halted = true;
     }
 
-    /// Create a new Intcode instance from the given string, and input noun and verb.
-    // pub fn new_with_input(s: &str, noun: usize, verb: usize) -> Self {
-    //     let mut ic = Self::new(s);
-    //
-    //     ic.memory[1] = noun;
-    //     ic.memory[2] = verb;
-    //     ic
-    // }
+    /// Create a new Intcode instance from the given string, and input.
+    pub fn new_with_input(s: &str, input: VecDeque<isize>) -> Self {
+        let mut ic = Self::new(s);
+
+        ic.input = input;
+        ic
+    }
 
     /// Create a new Intcode instance directly from the given string
     pub fn new(s: &str) -> Self {
@@ -67,14 +130,19 @@ impl Intcode {
             memory,
             pointer: 0,
             halted: false,
-            input: vec![],
-            output: vec![],
+            input: VecDeque::new(),
+            output: VecDeque::new(),
         }
     }
 
     /// Read an element of memory given an address
     pub fn read(&self, address: usize) -> isize {
         self.memory[address]
+    }
+
+    /// Get the output tape from the machine
+    pub fn get_output(&self) -> VecDeque<isize> {
+        self.output.clone()
     }
 
     /// Mutates the given memory in the memory tape to the given value
@@ -95,6 +163,35 @@ impl Intcode {
 
         s.pop();
         s
+    }
+}
+
+/// Given a modes string and an expected number of modes, returns a vector of Mode variants
+fn get_modes(digits: usize, num:  usize) -> Vec<Mode> {
+    let mut modes = Vec::new();
+    let mut modes_digits = digits;
+    for _ in 1..num {
+
+        if modes_digits % 10 == 1 {
+            modes.push(Mode::Immediate);
+        } else {
+            modes.push(Mode::Position);
+        }
+        modes_digits /= 10;
+    }
+    modes
+}
+
+
+fn parse_operation(n: isize) -> Operation {
+    let opcode: usize = n as usize % 100;
+    let modes_digits: usize = n as usize / 100;
+
+    let modes = get_modes(modes_digits, lengths[opcode]);
+
+    Operation {
+        opcode,
+        modes,
     }
 }
 
