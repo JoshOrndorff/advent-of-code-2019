4cdf4cdf5ba4d7eb5f61fc0ee4cb71951db41837
Joshy Orndorff <admin@joshyorndorff.com>
2020-09-17 00:58:56 -0400

    more pregress toward day 14

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,165 +1,221 @@
-use std::collections::{HashSet, BTreeMap};
+use std::collections::{BTreeMap, HashSet};
 mod parser;
 
-#[cfg(test)]
-mod tests;
-
 #[derive(Hash, Debug, PartialEq, Eq, Clone)]
-struct Reagent{
-	name: String,
-	quantity: u64,
+struct Reagent {
+	//TODO should I be using &str everywhere?
+    name: String,
+    quantity: u64,
 }
 
 //todo better name
 #[derive(Hash, Debug, PartialEq, Eq, Clone)]
 struct State {
-	reagents: BTreeMap<String, u64>,
+    reagents: BTreeMap<String, u64>,
+    ore_consumed: u64,
 }
 
 impl State {
-	fn new() -> Self {
-		Self { reagents: BTreeMap::new() }
-	}
+    fn new() -> Self {
+        Self {
+            reagents: BTreeMap::new(),
+			ore_consumed: 0,
+        }
+    }
 
-	fn is_all_ore(&self) -> bool {
-		self.reagents.keys().filter(|name| name != &"ORE").count() == 0
-	}
+    fn total_ore(&self) -> u64 {
+        match self.reagents.get("ORE".into()) {
+            Some(ore) => *ore,
+            None => 0,
+        }
+    }
 
-	fn total_ore(&self) -> u64 {
-		match self.reagents.get("ORE".into()) {
-			Some(ore) => *ore,
-			None => 0,
-		}
-	}
+    // fn get_prev_states(&self, reactions: &Vec<Reaction>) -> Vec<Self> {
+    // 	let mut neighbors = Vec::new();
+    // 	'reaction: for reaction in reactions {
+    // 		// Make a clone that we will mutate into the neighbor state
+    // 		let mut neighbor = self.clone();
+    //
+    // 		// Loop through the outputs subtracting them
+    // 		for output in &reaction.outputs {
+    // 			// If we don't have enough, then go on to the next reaction
+    // 			match neighbor.reagents.get(&output.name) {
+    // 				None => { continue 'reaction; },
+    // 				Some(&amount_we_have) => {
+    // 					if amount_we_have < output.quantity {
+    // 						continue 'reaction;
+    // 					}
+    // 					neighbor.reagents.insert(output.name.clone(), amount_we_have - output.quantity);
+    // 				}
+    // 			}
+    // 		}
+    //
+    // 		// If we made it throug hthe outputs without early returning, then this reaction is possible
+    // 		// so update the input quantities too
+    // 		for input in &reaction.inputs {
+    // 			let maybe_amount = neighbor.reagents.get(&input.name);
+    // 			match maybe_amount {
+    // 				None => { neighbor.reagents.insert(input.name.clone(), input.quantity); },
+    // 				Some(&amount_we_have) => {
+    // 					neighbor.reagents.insert(input.name.clone(), input.quantity + amount_we_have);
+    // 				}
+    // 			}
+    // 		}
+    //
+    // 		neighbors.push(neighbor);
+    // 	}
+    // 	neighbors
+    // }
 
-	fn get_prev_states(&self, reactions: &Vec<Reaction>) -> Vec<Self> {
-		let mut neighbors = Vec::new();
-		'reaction: for reaction in reactions {
-			// Make a clone that we will mutate into the neighbor state
-			let mut neighbor = self.clone();
+    fn get_next_state(&self, reaction: &Reaction) -> Option<Self> {
+        // Make a clone that we will mutate into the neighbor state
+        let mut neighbor = self.clone();
 
-			// Loop through the outputs subtracting them
-			for output in &reaction.outputs {
-				// If we don't have enough, then go on to the next reaction
-				match neighbor.reagents.get(&output.name) {
-					None => { continue 'reaction; },
-					Some(&amount_we_have) => {
-						if amount_we_have < output.quantity {
-							continue 'reaction;
-						}
-						neighbor.reagents.insert(output.name.clone(), amount_we_have - output.quantity);
-					}
-				}
-			}
+        // Loop through the inputs subtracting them
+        for input in &reaction.inputs {
+            // If we don't have enough, then exit early
+            match neighbor.reagents.get(&input.name) {
+                None => {
+                    return None;
+                }
+                Some(&amount_we_have) => {
+                    if amount_we_have < input.quantity {
+                        return None;
+                    }
+                    neighbor
+                        .reagents
+                        .insert(input.name.clone(), amount_we_have - input.quantity);
+                }
+            }
+        }
 
-			// If we made it throug hthe outputs without early returning, then this reaction is possible
-			// so update the input quantities too
-			for input in &reaction.inputs {
-				let maybe_amount = neighbor.reagents.get(&input.name);
-				match maybe_amount {
-					None => { neighbor.reagents.insert(input.name.clone(), input.quantity); },
-					Some(&amount_we_have) => {
-						neighbor.reagents.insert(input.name.clone(), input.quantity + amount_we_have);
-					}
-				}
-			}
+        // If we made it through the inputs without early returning, then this reaction is possible
+        // so update the output quantities too
+        for output in &reaction.outputs {
+            match neighbor.reagents.get(&output.name) {
+                None => {
+                    neighbor
+                        .reagents
+                        .insert(output.name.clone(), output.quantity);
+                }
+                Some(&amount_we_have) => {
+                    neighbor
+                        .reagents
+                        .insert(output.name.clone(), output.quantity + amount_we_have);
+                }
+            }
+        }
 
-			neighbors.push(neighbor);
-		}
-		neighbors
-	}
+        Some(neighbor)
+    }
 
-	fn get_next_states(&self, reactions: &Vec<Reaction>) -> Vec<Self> {
-		let mut neighbors = Vec::new();
-		'reaction: for reaction in reactions {
-			// Make a clone that we will mutate into the neighbor state
-			let mut neighbor = self.clone();
+    fn has_fuel(&self) -> bool {
+        match self.reagents.get("FUEL".into()) {
+            None => false,
+            Some(amount) => amount > &0,
+        }
+    }
 
-			// Loop through the inputs subtracting them
-			for input in &reaction.inputs {
-				// If we don't have enough, then go on to the next reaction
-				match neighbor.reagents.get(&input.name) {
-					None => { continue 'reaction; },
-					Some(&amount_we_have) => {
-						if amount_we_have < input.quantity {
-							continue 'reaction;
-						}
-						neighbor.reagents.insert(input.name.clone(), amount_we_have - input.quantity);
-					}
-				}
-			}
-
-			// If we made it through the inputs without early returning, then this reaction is possible
-			// so update the output quantities too
-			for output in &reaction.outputs {
-				match neighbor.reagents.get(&output.name) {
-					None => { neighbor.reagents.insert(output.name.clone(), output.quantity); },
-					Some(&amount_we_have) => {
-						neighbor.reagents.insert(output.name.clone(), output.quantity + amount_we_have);
-					}
-				}
-			}
-
-			neighbors.push(neighbor);
-		}
-		neighbors
-	}
-
-	fn has_fuel(&self) -> bool {
-		match self.reagents.get("FUEL".into()) {
-			None => false,
-			Some(amount) => amount > &0,
-		}
-	}
-
-	fn add(&mut self, name: String, quantity: u64) {
-		self.reagents.insert(name, quantity);
-	}
+    fn gather_ore(&self) -> Self {
+		let mut next_state = self.clone();
+        *next_state.reagents.entry("ORE".into()).or_insert(0) += 1;
+		next_state.ore_consumed += 1;
+		next_state
+    }
 }
 
 #[derive(Hash, Debug, PartialEq, Eq, Clone)]
 pub struct Reaction {
-	inputs: Vec<Reagent>,
-	outputs: Vec<Reagent>,
+    inputs: Vec<Reagent>,
+    outputs: Vec<Reagent>,
+}
+
+fn solve_part_1(reactions: &Vec<Reaction>) -> u64 {
+	// We'll solve the problem using Dijkstra's algorithm to find a path from no resources
+    // to one fules
+    let mut unexplored = HashSet::<State>::new();
+    let mut explored = HashSet::<State>::new();
+
+    // We'll find a path from the
+    // starting state (empty) to any valid target state (at least one fuel).
+    unexplored.insert(State::new());
+
+    let mut current_state = State::new();
+
+    while !current_state.has_fuel() {
+        println!("\n\nIn main dijkstra loop");
+
+        //TODO current_state is not currently a reference. Can it be?
+        // Get a reference to the next state to explore
+        current_state = unexplored
+            .iter()
+            .min_by(|x, y| x.ore_consumed.cmp(&y.ore_consumed))
+            .expect("Unexplored set should not be empty; min_by will return Some(_); qed;")
+            .clone();
+
+        println!("Current state: {:?}", current_state);
+
+        // Calculate any states we can transition to by applying a reaction
+        let neighbors = reactions
+            .iter()
+            .filter_map(|r| current_state.get_next_state(r))
+            .collect::<Vec<_>>();
+        println!("Found {} neighbors", neighbors.len());
+
+        // Mark the current state explored
+        explored.insert(unexplored.take(&current_state).unwrap());
+
+        // We also want to explore gathering more ore because some reactions
+		// need more than 1 ore.
+        unexplored.insert(current_state.gather_ore());
+
+        // Mark each neighbor as unexplored
+        for neighbor in neighbors {
+            println!("{:?}", neighbor);
+            unexplored.insert(neighbor);
+        }
+    }
+
+	current_state.ore_consumed
 }
 
 fn main() {
-	// Parse the puzzle input, which represents a set of reactions
-	let reactions = parser::parse_reactions("dummy");
+    // Parse the puzzle input, which represents a set of reactions
+    let reactions = parser::parse_reactions("dummy");
 
-	// We'll solve the problem using Dijkstra's algorithm to find a path from no resources
-	// to one fules
-	let mut unexplored = HashSet::<State>::new();
-	let mut explored = HashSet::<State>::new();
+	let part_1 = solve_part_1(&reactions);
 
-	// We'll find a path from the
-	// starting state (empty) to any valid target state (at least one fuel).
-	let mut start_state = State::new();
-	unexplored.insert(start_state);
-
-	let mut current_state = State::new();
-
-	while !current_state.has_fuel() {
-		println!("\n\nIn main dijkstra loop");
-
-		// Get a reference to the next state to explore
-		current_state = unexplored.iter().min_by(|x, y| x.total_ore().cmp(&y.total_ore())).expect("Fuck, the unexplored set must have been empty. That means no path.").clone();
-		println!("Current state: {:?}", current_state);
-
-
-		let neighbors = current_state.get_next_states(&reactions);
-		println!("Found {} neighbors", neighbors.len());
-
-		for neighbor in neighbors {
-			println!("{:?}", neighbor);
-			unexplored.insert(neighbor);
-		}
-
-		// Mark the current state explored
-		explored.insert(unexplored.take(&current_state).unwrap());
-
-		//TODO This is temp to only go through one iteration of Dijkstra's
-		// break;
-	}
-
+	println!("Minimal ORE needed: {}", part_1);
 }
+
+#[test]
+fn first_example() {
+	let input = "
+		10 ORE => 10 A
+		1 ORE => 1 B
+		7 A, 1 B => 1 C
+		7 A, 1 C => 1 D
+		7 A, 1 D => 1 E
+		7 A, 1 E => 1 FUEL
+	";
+
+	let reactions = parser::parse_reactions(input);
+
+	assert_eq!(solve_part_1(&reactions), 31);
+}
+
+// #[test]
+// fn second_example() {
+// 	let input = "\
+// 9 ORE => 2 A
+// 8 ORE => 3 B
+// 7 ORE => 5 C
+// 3 A, 4 B => 1 AB
+// 5 B, 7 C => 1 BC
+// 4 C, 1 A => 1 CA
+// 2 AB, 3 BC, 4 CA => 1 FUEL";
+//
+// 	let reactions = parser::parse_reactions(input);
+//
+// 	assert_eq!(solve_part_1(&reactions), 165);
+// }
--- day14/src/parser.rs
+++ day14/src/parser.rs
@@ -1,118 +1,132 @@
 use super::*;
+use std::str::FromStr;
+
+pub fn parse_reactions(s: &str) -> Vec<Reaction> {
+	s.trim().split('\n').map(parse_reaction).collect()
+}
 
 // Is this how you take a sting slice?
-// TODO actually do some parsing
-pub fn parse_reactions(_s: &str) -> Vec<Reaction> {
-	vec![
-		Reaction {
-			inputs: vec![
-				Reagent {
-					name: "ORE".into(),
-					quantity: 10,
-				},
-			],
-			outputs: vec![
-				Reagent {
-					name: "A".into(),
-					quantity: 10,
-				},
-			]
-		},
-		Reaction {
-			inputs: vec![
-				Reagent {
-					name: "ORE".into(),
-					quantity: 1,
-				},
-			],
-			outputs: vec![
-				Reagent {
-					name: "B".into(),
-					quantity: 1,
-				},
-			]
-		},
-		Reaction {
-			inputs: vec![
-				Reagent {
-					name: "A".into(),
-					quantity: 7,
-				},
-				Reagent {
-					name: "B".into(),
-					quantity: 1,
-				},
-			],
-			outputs: vec![
-				Reagent {
-					name: "C".into(),
-					quantity: 1,
-				},
-			]
-		},
-		Reaction {
-			inputs: vec![
-				Reagent {
-					name: "A".into(),
-					quantity: 7,
-				},
-				Reagent {
-					name: "C".into(),
-					quantity: 1,
-				},
-			],
-			outputs: vec![
-				Reagent {
-					name: "D".into(),
-					quantity: 1,
-				},
-			]
-		},
-		Reaction {
-			inputs: vec![
-				Reagent {
-					name: "A".into(),
-					quantity: 7,
-				},
-				Reagent {
-					name: "D".into(),
-					quantity: 1,
-				},
-			],
-			outputs: vec![
-				Reagent {
-					name: "E".into(),
-					quantity: 1,
-				},
-			]
-		},
-		Reaction {
-			inputs: vec![
-				Reagent {
-					name: "A".into(),
-					quantity: 7,
-				},
-				Reagent {
-					name: "E".into(),
-					quantity: 1,
-				},
-			],
-			outputs: vec![
-				Reagent {
-					name: "FUEL".into(),
-					quantity: 1,
-				},
-			]
-		},
-		// The special ore collection "reaction"
-		Reaction {
-			inputs: vec![],
-			outputs: vec![
-				Reagent {
-					name: "ORE".into(),
-					quantity: 1,
-				},
-			]
-		}
-	]
+fn parse_reaction(s: &str) -> Reaction {
+	let arrow_index = s.find("=>").expect("Arrow symbol is in every live");
+	let input_str = &s[..arrow_index].trim();
+	let output_str = &s[(arrow_index + 2)..].trim();
+
+	Reaction{
+		inputs: parse_reagents(input_str),
+		outputs: parse_reagents(output_str),
+	}
+}
+
+fn parse_reagents(s: &str) -> Vec<Reagent> {
+	s.split(",").map(|s| s.trim()).map(parse_reagent).collect()
+}
+
+fn parse_reagent(s: &str) -> Reagent {
+	let space_index = s.find(" ").expect("every reagent has a space between the quantity and the name");
+	let quantity_str = s[..space_index].trim();
+	let name_str = s[space_index..].trim();
+
+	Reagent {
+		name: name_str.into(),
+		quantity: FromStr::from_str(quantity_str).unwrap(),
+	}
+}
+
+#[test]
+fn first_example_parses() {
+	let input = "\
+10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL";
+
+    let expected = vec![
+        Reaction {
+            inputs: vec![Reagent {
+                name: "ORE".into(),
+                quantity: 10,
+            }],
+            outputs: vec![Reagent {
+                name: "A".into(),
+                quantity: 10,
+            }],
+        },
+        Reaction {
+            inputs: vec![Reagent {
+                name: "ORE".into(),
+                quantity: 1,
+            }],
+            outputs: vec![Reagent {
+                name: "B".into(),
+                quantity: 1,
+            }],
+        },
+        Reaction {
+            inputs: vec![
+                Reagent {
+                    name: "A".into(),
+                    quantity: 7,
+                },
+                Reagent {
+                    name: "B".into(),
+                    quantity: 1,
+                },
+            ],
+            outputs: vec![Reagent {
+                name: "C".into(),
+                quantity: 1,
+            }],
+        },
+        Reaction {
+            inputs: vec![
+                Reagent {
+                    name: "A".into(),
+                    quantity: 7,
+                },
+                Reagent {
+                    name: "C".into(),
+                    quantity: 1,
+                },
+            ],
+            outputs: vec![Reagent {
+                name: "D".into(),
+                quantity: 1,
+            }],
+        },
+        Reaction {
+            inputs: vec![
+                Reagent {
+                    name: "A".into(),
+                    quantity: 7,
+                },
+                Reagent {
+                    name: "D".into(),
+                    quantity: 1,
+                },
+            ],
+            outputs: vec![Reagent {
+                name: "E".into(),
+                quantity: 1,
+            }],
+        },
+        Reaction {
+            inputs: vec![
+                Reagent {
+                    name: "A".into(),
+                    quantity: 7,
+                },
+                Reagent {
+                    name: "E".into(),
+                    quantity: 1,
+                },
+            ],
+            outputs: vec![Reagent {
+                name: "FUEL".into(),
+                quantity: 1,
+            }],
+        },
+    ];
+	assert_eq!(parse_reactions(input), expected);
 }
